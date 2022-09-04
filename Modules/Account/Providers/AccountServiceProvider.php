894502499d63b95cd1da41702e01623ddbf418bf
Kos Huang <koshuang@gmail.com>
2022-09-04 13:34:34 +0800

    refactor(account): move Config into Infrastructure

--- Modules/Account/Providers/AccountServiceProvider.php
+++ Modules/Account/Providers/AccountServiceProvider.php
@@ -49,10 +49,10 @@ class AccountServiceProvider extends ServiceProvider
     protected function registerConfig()
     {
         $this->publishes([
-            module_path($this->moduleName, 'Config/config.php') => config_path($this->moduleNameLower . '.php'),
+            module_path($this->moduleName, 'Infrastructure/Config/config.php') => config_path($this->moduleNameLower . '.php'),
         ], 'config');
         $this->mergeConfigFrom(
-            module_path($this->moduleName, 'Config/config.php'), $this->moduleNameLower
+            module_path($this->moduleName, 'Infrastructure/Config/config.php'), $this->moduleNameLower
         );
     }
 
