4b2d159a138a3155dbbe28bbff8455102fb82e19
Kos Huang <koshuang@gmail.com>
2022-09-10 16:37:59 +0800

    chore(account/adapter): [AccountPersistenceAdapter] add loadAccount()

---
+++ Modules/Account/Infrastructure/Adapter/Out/Persistence/AccountMapper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Modules\Account\Infrastructure\Adapter\Out\Persistence;
+
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Modules\Account\Domain\ActivityWindow;
+use Modules\Account\Domain\Entities\Account;
+use Modules\Account\Domain\Entities\Activity;
+use Modules\Account\Domain\ValueObjects\AccountId;
+use Modules\Account\Domain\ValueObjects\ActivityId;
+use Modules\Account\Domain\ValueObjects\Money;
+use Modules\Account\Infrastructure\Adapter\Out\Persistence\Models\AccountModel;
+use Modules\Account\Infrastructure\Adapter\Out\Persistence\Models\ActivityModel;
+
+class AccountMapper
+{
+    /**
+     * @param  Collection<ActivityModel>  $activityModels
+     */
+    public function mapToDomainEntity(
+        AccountModel $accountModel,
+        Collection $activityModels,
+        int $withdrawalBalance,
+        int $depositBalance,
+    ): Account {
+        $baselineBalance = Money::subtract(
+            Money::of($depositBalance),
+            Money::of($withdrawalBalance),
+        );
+
+        return Account::withId(
+            new AccountId($accountModel->id),
+            $baselineBalance,
+            $this->mapToActivityWindow($activityModels),
+        );
+    }
+
+    /**
+     * @param  Collection<ActivityModel>  $activityModels
+     */
+    public function mapToActivityWindow(Collection $activityModels): ActivityWindow
+    {
+        $activityEntities = $activityModels->map(fn (ActivityModel $activity) => new Activity(
+            new ActivityId($activity->id),
+            new AccountId($activity->owner_account_id),
+            new AccountId($activity->source_account_id),
+            new AccountId($activity->target_account_id),
+            Carbon::parse($activity->created_at),
+            Money::of($activity->amount),
+        ));
+
+        return new ActivityWindow(...$activityEntities);
+    }
+}
---
+++ Modules/Account/Infrastructure/Adapter/Out/Persistence/AccountPersistenceAdapter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Modules\Account\Infrastructure\Adapter\Out\Persistence;
+
+use Carbon\Carbon;
+use Modules\Account\Application\Port\Out\LoadAccountPort;
+use Modules\Account\Application\Port\Out\UpdateAccountStatePort;
+use Modules\Account\Domain\Entities\Account;
+use Modules\Account\Domain\ValueObjects\AccountId;
+use Modules\Account\Infrastructure\Adapter\Out\Persistence\Models\AccountModel;
+use Modules\Account\Infrastructure\Adapter\Out\Persistence\Models\ActivityModel;
+
+class AccountPersistenceAdapter implements LoadAccountPort, UpdateAccountStatePort
+{
+    private AccountMapper $accountMapper;
+
+    public function __construct(AccountMapper $accountMapper)
+    {
+        $this->accountMapper = $accountMapper;
+    }
+
+    public function loadAccount(AccountId $accountId, Carbon $baselineDate): Account
+    {
+        $accountModel = AccountModel::findOrFail($accountId->value);
+        $activityModels = ActivityModel::where([
+            ['owner_account_id', $accountId->value],
+            ['created_at', '>=', $baselineDate->toDateTimeString()],
+        ])->get();
+
+        $withdrawalBalance = ActivityModel::where([
+            ['owner_account_id', $accountId->value],
+            ['source_account_id', $accountId->value],
+            ['created_at', '<', $baselineDate->toDateTimeString()],
+        ])->sum('amount');
+
+        $depositBalance = ActivityModel::where([
+            ['owner_account_id', $accountId->value],
+            ['target_account_id', $accountId->value],
+            ['created_at', '<', $baselineDate->toDateTimeString()],
+        ])->sum('amount');
+
+        return $this->accountMapper->mapToDomainEntity(
+            $accountModel,
+            $activityModels,
+            $withdrawalBalance,
+            $depositBalance
+        );
+    }
+
+    public function updateActivities(Account $account): void
+    {
+    }
+}
